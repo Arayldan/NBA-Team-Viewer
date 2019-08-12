26af825eb6de458c220510293ae2294be2683486
Etienne Cuvillon <etienne.cuvillon@ebox.ca>
2019-08-12 19:05:54 -0400

    Ktlint format

--- app/src/main/java/ca/cuvillon/nbateamviewer/MainActivity.kt
+++ app/src/main/java/ca/cuvillon/nbateamviewer/MainActivity.kt
@@ -1,7 +1,7 @@
 package ca.cuvillon.nbateamviewer
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
