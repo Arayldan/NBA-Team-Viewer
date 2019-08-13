5e49df9a25594c704a95dd0ee4c39de61392453d
Etienne Cuvillon <etienne.cuvillon@ebox.ca>
2019-08-13 18:00:40 -0400

    Navigation: Implement navigation from ViewModel

--- common/src/main/java/ca/cuvillon/common/base/BaseFragment.kt
+++ common/src/main/java/ca/cuvillon/common/base/BaseFragment.kt
@@ -1,8 +1,38 @@
 package ca.cuvillon.common.base
 
+import android.os.Bundle
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.Observer
+import androidx.navigation.fragment.FragmentNavigatorExtras
+import androidx.navigation.fragment.findNavController
+import ca.cuvillon.common.utils.Event
+import ca.cuvillon.navigation.NavigationCommand
 
 abstract class BaseFragment : Fragment() {
 
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        observeNavigation(getViewModel())
+    }
+
     abstract fun getViewModel(): BaseViewModel
+
+    /**
+     * Observe a [NavigationCommand] [Event] [LiveData].
+     * When this [LiveData] is updated, [Fragment] will navigate to its destination.
+     */
+    private fun observeNavigation(viewModel: BaseViewModel) {
+        viewModel.navigation.observe(viewLifecycleOwner, Observer {
+            it?.getContentIfNotHandled()?.let { command ->
+                when (command) {
+                    is NavigationCommand.To -> findNavController().navigate(
+                        command.directions,
+                        FragmentNavigatorExtras()
+                    )
+                    is NavigationCommand.Back -> findNavController().navigateUp()
+                }
+            }
+        })
+    }
 }
--- common/src/main/java/ca/cuvillon/common/base/BaseViewModel.kt
+++ common/src/main/java/ca/cuvillon/common/base/BaseViewModel.kt
@@ -1,5 +1,21 @@
 package ca.cuvillon.common.base
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.navigation.NavDirections
+import ca.cuvillon.common.utils.Event
+import ca.cuvillon.navigation.NavigationCommand
 
-abstract class BaseViewModel : ViewModel()
+abstract class BaseViewModel : ViewModel() {
+    // NAVIGATION
+    private val _navigation = MutableLiveData<Event<NavigationCommand>>()
+    val navigation: LiveData<Event<NavigationCommand>> = _navigation
+
+    /**
+     * Handle navigation directly from [ViewModel].
+     */
+    fun navigate(directions: NavDirections) {
+        _navigation.value = Event(NavigationCommand.To(directions))
+    }
+}
